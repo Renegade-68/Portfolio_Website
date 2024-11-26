76997281938ca498a08e9e9315cb4d7843537d2a
Reno Zanders <onersrednaz@outlook.com>
2024-11-26 23:35:20 +0100

    Portfolio Site V0.1
    Added:
    Header with placeholder
    Buttons to pages
    Initialized styling
    Added font
    Added fontawesome icons
    Added Bootstrap V5.3

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php 
+echo "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!-- This is the frontpage -->
+        <title>Home</title>
+
+        <!-- Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&display=swap" rel="stylesheet">
+
+
+        <!-- Fontawesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+        <!-- Styling -->
+        <link rel="stylesheet" href="./assets/css/style.css">
+    </head>
+
+    <body>
+        
+        <div class="container-fluid g-0">
+
+            <div class="index_header">
+                <h1>[placeholder]</h1>
+            </div>
+
+            <div class="nav">
+                <button class="navBtn green">About Me</button>
+                <button class="navBtn red">Projects</button>
+                <button class="navBtn yellow">Internships</button>
+                <button class="navBtn blue">Contact Me</button>
+            </div>
+
+            <div class="navLine">
+                <div class="navColors green"><i class="fa-solid fa-caret-down caret"></i></div>
+                <div class="navColors red"><i class="fa-solid fa-caret-down caret"></i></div>
+                <div class="navColors yellow"><i class="fa-solid fa-caret-down caret"></i></div>
+                <div class="navColors blue"><i class="fa-solid fa-caret-down caret"></i></div>
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
