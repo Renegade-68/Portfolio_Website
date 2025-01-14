19a5f2713ec2531a6715db6c6cff145127c5ee51
Reno Zanders <onersrednaz@outlook.com>
2025-01-14 23:18:16 +0100

    Portfolio Site V0.3
    Added:
    - Study part of the page
    - Contact part of the page with form
    
    Other:
    - Added icons
    - Fixed the image scaling

--- index.php
+++ index.php
@@ -39,21 +39,21 @@ echo "";
             <!-- This is the navigation for all pages -->
             <div class="nav">
                 <div class="navBtns">
-                    <a href="#" class="navBtn green">About Me<i class="fa-solid fa-caret-down caret"></i></a>
-                    <a href="#" class="navBtn red">Projects<i class="fa-solid fa-caret-down caret"></i></a>
-                    <a href="#" class="navBtn yellow">Study<i class="fa-solid fa-caret-down caret"></i></a>
-                    <a href="#" class="navBtn blue">Contact Me<i class="fa-solid fa-caret-down caret"></i></a>
+                    <a href="#" class="green"><p><i class="fa-solid fa-address-card"></i> About</p><i class="fa-solid fa-caret-down caret"></i></a>
+                    <a href="#" class="red"><p><i class="fa-solid fa-diagram-project"></i> Projects</p><i class="fa-solid fa-caret-down caret"></i></a>
+                    <a href="#" class="yellow"><p><i class="fa-solid fa-graduation-cap"></i> Study</p><i class="fa-solid fa-caret-down caret"></i></a>
+                    <a href="#" class="blue"><p><i class="fa-solid fa-comment"></i> Contact</p><i class="fa-solid fa-caret-down caret"></i></a>
                 </div>
             </div>
 
-            <!-- The About Me section of the page! -->
+            <!-- The About section of the page! -->
             <div class="aboutmeContainer">
 
                 <div class="aboutmeTextContainer">
-                    <h1>About Me</h1> <br>
+                    <h1>About <i class="fa-solid fa-address-card"></i></h1> <br>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil assumenda quidem sint dolore nobis. Laborum obcaecati, repudiandae exercitationem dolorum qui ut quo quas temporibus suscipit quos cum repellendus inventore modi, unde voluptates maxime perspiciatis corporis mollitia ab ipsum iste rerum impedit! Impedit, labore. Blanditiis, ab quidem mollitia esse officiis corrupti molestiae, quam dolorem cumque doloribus ducimus eveniet quia debitis magni harum iusto! Harum vel, ab sequi reiciendis natus et voluptas dolorum temporibus est praesentium debitis ad quos mollitia odit voluptatem aliquam? Consectetur sunt quam illum quis rerum repellat, eum saepe aperiam. Similique laudantium, recusandae voluptas necessitatibus dolorum rem ut reprehenderit.</p>
                 </div>
-                
+
                 <div class="aboutmePhoto">
                     <img src="./assets/img/me.png" alt="me.png">
                 </div>
@@ -64,7 +64,7 @@ echo "";
             <div class="projectsContainer">
 
                 <div class="projectTextContainer">
-                    <h1>Projects</h1> <br>
+                    <h1>Projects <i class="fa-solid fa-diagram-project"></i></h1> <br>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fugit ab vel voluptatem voluptates, reiciendis ullam ea, obcaecati optio dicta illum deleniti sint assumenda atque fuga. Minus atque sunt vel aliquam nostrum quisquam veniam placeat consequuntur ea voluptates? Quaerat suscipit odit eveniet repellendus provident nobis optio eius est iure porro.</p>
                 </div>
 
@@ -91,6 +91,118 @@ echo "";
                         <a href="#">Read More <i class="fa-solid fa-arrow-right"></i></a>
 
                     </div>
+
+                </div>
+
+            </div>
+
+            <!-- The Study section of the page! -->
+            <div class="studyContainer">
+
+                <div class="studyTextContainer">
+                    <h1>Study <i class="fa-solid fa-graduation-cap"></i></h1> <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatem laudantium laboriosam provident beatae id possimus veritatis facere quasi nulla, totam aut dicta fugiat, reiciendis vel vitae magni minus obcaecati! Tempora nulla totam, reiciendis nemo harum blanditiis cupiditate dolor. At natus magni eum provident eligendi consequuntur veritatis eos, iste doloremque quaerat commodi, facilis aliquam hic optio quae cumque unde iusto aperiam ipsam labore architecto nobis id! Inventore quibusdam saepe rem ipsum ex, fugit ratione perspiciatis eligendi asperiores neque fugiat quisquam esse quis harum veritatis velit laborum quam obcaecati incidunt ut consectetur in accusantium veniam. Obcaecati consectetur vitae eligendi quidem dolor suscipit alias iure illum asperiores, accusantium, fugit tempora, quae nam! Nam, modi. Aut tempora vitae dolores mollitia officia deleniti, at alias voluptatibus odit autem laudantium nulla esse molestias debitis illum ad ratione nesciunt nisi, quos voluptatum in qui fuga atque? Laudantium vero iure quas ipsam maiores inventore excepturi eligendi magnam!</p>
+                </div>
+
+                <div class="studyBlockContainer">
+
+                    <div class="studyBlock">
+                        <h2>Study Title</h2>
+                        <h3>Year</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corporis deleniti nam, consequatur omnis perspiciatis!</p>
+                        <a href="#">Read More <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+
+                    <div class="studyBlock">
+                        <h2>Study Title</h2>
+                        <h3>Year</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corporis deleniti nam, consequatur omnis perspiciatis!</p>
+                        <a href="#">Read More <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+
+                    <div class="studyBlock">
+                        <h2>Study Title</h2>
+                        <h3>Year</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corporis deleniti nam, consequatur omnis perspiciatis!</p>
+                        <a href="#">Read More <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- The Contact section of the page! -->
+            <div class="contactContainer">
+
+                <div class="contactTextContainer">
+
+                    <div class="contactText">
+                        <h1>Contact <i class="fa-solid fa-comment"></i></h1><br>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nisi maiores eum placeat vel quae impedit temporibus unde incidunt totam nobis assumenda provident obcaecati libero quam, quia minus odit? Dolore optio eos magni rem tempora cumque perspiciatis modi eaque, dolores, nobis alias adipisci ad? Quisquam modi explicabo quam voluptatem optio natus sint, nisi corrupti reiciendis dolorum nam necessitatibus quibusdam itaque quod dolor debitis ullam doloremque. Delectus sint incidunt cumque! Ea.</p>
+                    </div>
+
+                    <div class="contactContactinfo">
+                        <div class="contactSocials">
+                            <h1>Socials</h1>
+                            <h2><p><i class="fa-brands fa-linkedin" style="color: #3A5C43"></i> LinkedIn</p></h2>
+                            <h2><p><i class="fa-brands fa-github" style="color: #9C4035"></i> GitHub</p></h2>
+                            <h2><p><i class="fa-brands fa-instagram" style="color: #C88630"></i> Instagram</p></h2>
+                            <h2><p><i class="fa-brands fa-reddit" style="color: #385D65"></i> Reddit</p></h2>
+                        </div>
+                        <div class="contactCreds">
+                            <h1>Contactinfo</h1>
+                            <h2><p><i class="fa-brands fa-discord" style="color: #3A5C43"></i> @rummy03</p></h2>
+                            <h2><p><i class="fa-solid fa-phone" style="color: #9C4035"></i> +31 06-19268306</p></h2>
+                            <h2><p><i class="fa-solid fa-envelope" style="color: #C88630"></i> reno.zanders@outlook.com</p></h2>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="contactFormContainer">
+
+                    <div class="formText">
+                        <h1>Of stuur een berichtje hier!</h1><br>
+                    </div>
+
+                    <form action="" method="post">
+
+                        <div class="formBox">
+                            <div class="formPart">
+                                <label for="fname"><i class="fa-solid fa-user"></i> Voornaam:</label> <br>
+                                    <input type="text" id="fname" name="fname" placeholder="John"> <br> <br>
+                            </div>
+                            <div class="formPart">
+                                <label for="fname"><i class="fa-solid fa-user"></i> Achternaam:</label> <br>
+                                    <input type="text" id="lname" name="lname" placeholder="Doe"> <br> <br>
+                            </div>
+                        </div>
+
+                        <div class="formBox">
+                            <div class="formPart">
+                                <label for="email"><i class="fa-solid fa-envelope"></i> Email:</label> <br>
+                                    <input type="text" id="email" name="email" placeholder="john-doe@mail.nl"> <br> <br>
+                            </div>
+                            <div class="formPart">
+                                <label for="phone"><i class="fa-solid fa-phone"></i> Telefoonnummer:</label> <br>
+                                    <input type="text" id="phone" name="phone" placeholder="06-12345678"> <br> <br>
+                            </div>
+                        </div>
+
+                        <div class="formBox">
+                            <div class="formPart">
+                                <label for="msg"><i class="fa-solid fa-comment"></i> Bericht:</label> <br>
+                                    <textarea name="msg" id="msg" placeholder="Bericht..."></textarea> <br> <br>
+                            </div>
+                        </div>
+
+                        <div class="formBox">
+                            <div class="formSend">
+                                <button>Versturen <i style="background-color: inherit;" class="fa-solid fa-paper-plane"></i></button>
+                            </div>
+                        </div>
+                    
+                    </form>
 
                 </div>
 
